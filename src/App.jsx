22d0c78683ef18a0374915b17563f3c476fb6920
leonardoXLT <148663990+leonardoXLT@users.noreply.github.com>
2024-08-18 22:06:21 +1000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 
 
 export default function App() {
-  return <BrowserRouter>
+  return (
+  <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
@@ -16,5 +19,6 @@ export default function App() {
       <Route path="/about" element={<About />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
-    </BrowserRouter>
-}
+  </BrowserRouter>
+  )
+};
